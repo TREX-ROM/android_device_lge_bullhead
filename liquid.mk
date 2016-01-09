4b54a9ddec466e2087ffacf9909a0dfab35e7314
LiquidDark <fisch890@gmail.com>
2016-01-09 10:07:30 +0100

    update Device identifier & specific prebuilt files
    PRODUCT_BRAND
    PRODUCT_MODEL
    BUILD_FINGERPRINT

--- liquid.mk
+++ liquid.mk
@@ -11,16 +11,21 @@ $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
+# Copy specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/liquid/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/liquid/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := liquid_bullhead
-PRODUCT_BRAND := google
-PRODUCT_MODEL := Nexus 5x
-TARGET_MANUFACTURER := LGE
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
 
 TARGET_VENDOR := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0.1/MMB29K/2419427:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29K 2419427 release-key" \
-    BUILD_ID=MMB29K
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29M/2431559:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29M 2431559 release-keys"
